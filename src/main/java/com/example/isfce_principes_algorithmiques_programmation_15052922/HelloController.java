21fb54bb797993cd857d0a535dc5d5958adf0ffd
thomas-ayissi <thomas8sea@gmail.com>
2022-05-15 20:42:50 +0200

    commit

---
+++ src/main/java/com/example/isfce_principes_algorithmiques_programmation_15052922/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.isfce_principes_algorithmiques_programmation_15052922;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
